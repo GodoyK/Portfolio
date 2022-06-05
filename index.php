0ec4c3d2a1cdd01d923f431f2949f1aa2c2e5972
GodoyK <contatodeinternet@gmail.com>
2022-06-05 13:52:50 -0300

    arquivos para o site Kelly Godoy

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf8">
+		<title>Kelly Godoy - Site Oficial</title>
+			<link rel="stylesheet" href="style.css">
+				<script src="https://code.jquery.com/jquery-3.6.0.js" 
+					integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" 
+					crossorigin="anonymous">
+				</script>
+	</head>
+	
+	<body>
+	
+		<header>
+			<img src="kelly-godoy_01.jpg" alt="Foto do rosto de Kelly Godoy">
+				<h1>Kelly Godoy</h1>
+		</header>
+		<section>   </section>
+			<header>   </header>
+			<article>
+				<header>   
+					<h3 class= "post_title"> Post #1 </h3>
+					<img src="kelly-godoy_01." alt ="">
+				</header>
+				<p>   </p>
+			</article>
+		</section>
+		<footer>   </footer>
+	
+	</body>
+	
+
+	
+	
+</html>
